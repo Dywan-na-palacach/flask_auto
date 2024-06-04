024ecc0a8d1dbdf941b3aaa93be4534ef336095d
Dywan-na-palacach <130796921+Dywan-na-palacach@users.noreply.github.com>
2024-06-04 17:48:16 +0200

    app.py

--- app.py
+++ app.py
@@ -1,6 +1,14 @@
-from flask import Flask
-app = Flask(__name__)
+from flask import Flask, request
+from flask_restful import Resource, Api
+import sys
+import os
 
+app = Flask(__name__)
+api = Api(app)
+port = 5100
+
+print("Api running on port : {} ".format(port)
+      
 @app.route('/')
 def hello_world():
     return 'hello world'
