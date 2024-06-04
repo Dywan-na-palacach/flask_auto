f99f889d30b36ed329124f5469fa46ac140cac38
Dywan-na-palacach <130796921+Dywan-na-palacach@users.noreply.github.com>
2024-06-04 17:33:12 +0200

    app.py

--- app.py
+++ app.py
@@ -1,1 +1,9 @@
-print(f'Hello world')
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'hello world'
+
+if __name__ == '__main__':
+    app.run(debug=True)
